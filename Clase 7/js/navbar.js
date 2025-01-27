478a4fd6cd1ec7c6901b95b7d19b71c2c681ebb6
George <george72001@gmail.com>
2025-01-27 13:01:25 -0600

    Cambios en la clase 7, ademas de agregar la carpeta para la clase 8

--- Clase 7/js/navbar.js
+++ Clase 7/js/navbar.js
@@ -1,16 +1,17 @@
-let menu = `<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+const menu = `
+              <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="/index.html">Tienda en Línea</a>
+                  <a class="navbar-brand" href="index.html">Tienda en Línea</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/catalogo.html">Catalogo</a>
+                        <a class="nav-link active" aria-current="page" href="catalogo.html">Catalogo</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" href="/agregarInventario.html">Agregar Inventario</a>
+                        <a class="nav-link active" href="agregarInventario.html">Agregar Inventario</a>
                       </li>
                     </ul>
                   </div>
